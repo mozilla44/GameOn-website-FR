5adc57dcd4db07054a9ccdc3d1dd75cba265843f
antoine <antoine.frisco@gmail.com>
2021-05-26 15:32:01 +0200

    nom + prenom

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -52,8 +52,8 @@ let regexBirthdate = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4
 let regexQuantity= /^\d{1,2}$/;
 
 //test regex
-let testFirst = regexName.test(firstName)
-let testLast = regexName.test(lastName)
+let testFirst = regexName.test(document.getElementById("first").value)
+let testLast = regexName.test(document.getElementById("last").value)
 let testEmail = regexEmail.test(emailAdress)
 let testBirthdate = regexBirthdate.test(birthDate)
 let testQuantity = regexQuantity.test(numberOfEvents)
@@ -69,43 +69,22 @@ document.getElementById("checkbox1").addEventListener("change")
 //validate form fuction 
 
  
-function validate (){
-  
-  if (testFirst == false){
-    document.getElementById("errorFirst").style.display = "block";
-    document.getElementById("errorFirst").innerHTML = "Veuillez entrer un prénom valide";
-    document.getElementById("first").style.border = "2px solid red";
-  }
+function validate(){
+	let regexName = /^\D{2,}$/;
+	let testFirst = regexName.test(document.getElementById("first").value);
+  let testLast = regexName.test(document.getElementById("last").value);
+	if (testFirst == false){
+    	document.getElementById("errorFirst").innerHTML = "Veuillez entrer un prénom valide";
+    	document.getElementById("first").style.border = "2px solid red";
+  	}
+		else {document.getElementById("errorFirst").style.display = "none"; document.getElementById("first").style.border = "2px solid green";}
   if (testLast == false){
-    document.getElementById("errorLast").style.display = "block";
-    document.getElementById("errorLast").innerHTML = "Veuillez entrer un nom valide";
+    document.getElementById("errorLast").innerHTML ="veuillez entrer un nom valide";
     document.getElementById("last").style.border = "2px solid red";
   }
-  if (testEmail == false){
-    document.getElementById("errorEmail").style.display = "block";
-    document.getElementById("errorEmail").innerHTML = "Veuillez entrer une adresse e-mail valide";
-    document.getElementById("email").style.border = "2px solid red";
-  }
-  if (testBirthdate == false){
-    document.getElementById("errorBirthdate").style.display = "block";
-    document.getElementById("errorBirthdate").innerHTML = "Veuillez entrer une date valide";
-    document.getElementById("birthdate").style.border = "2px solid red";
-  }
-  if (testQuantity == false){
-    document.getElementById("errorQuantity").style.display = "block";
-    document.getElementById("errorQuantity").innerHTML = "Veuillez entrer un nombre valide";
-    document.getElementById("quantity").style.border = "2px solid red";
-  }
-  if (document.getElementById("checkbox1").checked == false){
-    document.getElementById("checkboxRequired").style.display = "block";
-    document.getElementById("checkboxRequired").innerHTML = "Veuillez accepter les conditions générales";
-  }
-  if (testFirst == false || testLast == false || testEmail == false || testQuantity == false || testBirthdate == false || document.getElementById("checkbox1").checked == false){
-    return false
-  }
-  /* if (testFirst == true && testLast == true && testEmail == true && testQuantity == true && testBirthdate == true && document.getElementById("checkbox1").checked == true){
-    return true
-  } */
+  else {
+    document.getElementById("errorLast").style.display = "none" , document.getElementById("last").style.border = "2px solid green"
+  }  
 }
 
  
