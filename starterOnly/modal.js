4706a58ffdc7469165ea4ff007f433aec29aa256
antoine <antoine.frisco@gmail.com>
2021-05-26 15:42:05 +0200

    email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -46,14 +46,14 @@ let numberOfEvents = document.getElementById("quantity").value;
 
 
 // regex 
-let regexName = /^\D{2,}$/;
+/* let regexName = /^\D{2,}$/; */
 let regexEmail = /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/;
 let regexBirthdate = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/
 let regexQuantity= /^\d{1,2}$/;
 
 //test regex
-let testFirst = regexName.test(document.getElementById("first").value)
-let testLast = regexName.test(document.getElementById("last").value)
+/* let testFirst = regexName.test(document.getElementById("first").value)
+let testLast = regexName.test(document.getElementById("last").value) */
 let testEmail = regexEmail.test(emailAdress)
 let testBirthdate = regexBirthdate.test(birthDate)
 let testQuantity = regexQuantity.test(numberOfEvents)
@@ -71,20 +71,31 @@ document.getElementById("checkbox1").addEventListener("change")
  
 function validate(){
 	let regexName = /^\D{2,}$/;
+  let regexEmail = /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/;
 	let testFirst = regexName.test(document.getElementById("first").value);
   let testLast = regexName.test(document.getElementById("last").value);
+  let testEmail = regexEmail.test(document.getElementById("email").value)
+
 	if (testFirst == false){
     	document.getElementById("errorFirst").innerHTML = "Veuillez entrer un prénom valide";
     	document.getElementById("first").style.border = "2px solid red";
   	}
 		else {document.getElementById("errorFirst").style.display = "none"; document.getElementById("first").style.border = "2px solid green";}
   if (testLast == false){
-    document.getElementById("errorLast").innerHTML ="veuillez entrer un nom valide";
+    document.getElementById("errorLast").innerHTML = "veuillez entrer un nom valide";
     document.getElementById("last").style.border = "2px solid red";
   }
   else {
     document.getElementById("errorLast").style.display = "none" , document.getElementById("last").style.border = "2px solid green"
-  }  
+  }
+  if (testEmail == false){
+    document.getElementById("errorEmail").innerHTML = "veuillez entrer une adresse email valide"
+    document.getElementById("email").style.border = "2px solid red"
+  }
+  else {
+    document.getElementById("errorEmail").style.display = "none" , document.getElementById("email").style.border = "2px solid green"
+  }
+  
 }
 
  
